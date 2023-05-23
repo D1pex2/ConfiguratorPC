54c6c6855b368dc0f4aade9da482b87b7fa71926
D1pex2 <kolya.shef2003@gmail.com>
2023-05-23 16:28:55 +0300

    helppage

--- ConfiguratorPC/ConfiguratorPC/Properties/Resources.Designer.cs
+++ ConfiguratorPC/ConfiguratorPC/Properties/Resources.Designer.cs
@@ -173,6 +173,16 @@ namespace ConfiguratorPC.Properties {
         /// <summary>
         ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap help_image7 {
+            get {
+                object obj = ResourceManager.GetObject("help_image7", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap hide {
             get {
                 object obj = ResourceManager.GetObject("hide", resourceCulture);
