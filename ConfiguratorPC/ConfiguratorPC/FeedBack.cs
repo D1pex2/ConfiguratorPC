8ae10f54eeb8de4cd4230a81086243f17be20bde
D1pex2 <kolya.shef2003@gmail.com>
2023-06-05 15:55:07 +0300

    п

--- ConfiguratorPC/ConfiguratorPC/FeedBack.cs
+++ ConfiguratorPC/ConfiguratorPC/FeedBack.cs
@@ -10,11 +10,17 @@ namespace ConfiguratorPC
 {
     public static class FeedBack
     {
+        /// <summary>
+        /// Метод отображения сообщения об ошибке
+        /// </summary>
+        /// <param name="ex">Исключение</param>
         public static void ShowError(Exception ex)
         {
+            //Проверка на исключение БД
             if (ex is EntityException)
             {
                 FeedBack.ShowError("Ошибка подключения к базе данных. Обратитесь к системному администратору.");
+                //Завершение работы приложения
                 Application.Current.Shutdown();
             }
             else
@@ -23,11 +29,19 @@ namespace ConfiguratorPC
             }
         }
 
+        /// <summary>
+        /// Метод отображения сообщения об ошибке
+        /// </summary>
+        /// <param name="message">Сообщение</param>
         public static void ShowError(string message)
         {
             new MessageWindow("Ошибка", message, true).ShowDialog();
         }
 
+        /// <summary>
+        /// Метод отображения сообщения
+        /// </summary>
+        /// <param name="message">Сообщение</param>
         public static void ShowMessage(string message)
         {
             new MessageWindow("Сообщение", message).ShowDialog();
